2d12a5e90d0c78c7d2092b1405c5ea072c28f7f6
Elio maia da Silva Junior <99017433+ejunior89@users.noreply.github.com>
2023-10-08 11:23:21 -0300

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,8 +13,6 @@ function updateProfileInfo(profileData) {
     const name = document.getElementById('profile.name');
     name.innerText = profileData.name;
 
-    const job = document.getElementById('profile.job');
-    job.innerText = profileData.job;
 
     const location = document.getElementById('profile.location');
     location.innerText = profileData.location;
@@ -55,18 +53,6 @@ function updatePortfolio(profileData) {
     }).join('');
 }
 
-function updateProfessionalExperience(profileData) {
-    const professionalExperience = document.getElementById('profile.professionalExperience');
-    professionalExperience.innerHTML = profileData.professionalExperience.map(experience => {
-        return `
-            <li>
-                <h3 class="title">${experience.name}</h3>
-                <p class="period">${experience.period}</p>
-                <p>${experience.description}</p>
-            </li>
-        `;
-    }).join('');
-}
 
 async function loadProfileData() {
     try {
